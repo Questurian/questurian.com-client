44c51e1e18759020715ebc97b7952094e888b352
alantothe <rubenamalpartida@gmail.com>
2025-11-16 23:32:57 -0500

    build

--- src/app/api/[[...path]]/route.ts
+++ src/app/api/[[...path]]/route.ts
@@ -6,7 +6,7 @@
 
 import { NextRequest, NextResponse } from 'next/server';
 
-export async function handleRequest(
+async function handleRequest(
   method: string,
   request: NextRequest,
   pathSegments: string[]
